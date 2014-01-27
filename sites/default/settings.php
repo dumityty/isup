05343d9035e091aff7b689821b87f19bc2fe30ea
dumityty <dumityty@yahoo.com>
2014-01-27 22:38:28 +0000

    move modules in contrib, custom; isup modules

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -552,7 +552,7 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  */
 # $conf['allow_authorize_operations'] = FALSE;
 
-$work = TRUE;
+$work = FALSE;
 
 $databases = array (
   'default' => 
